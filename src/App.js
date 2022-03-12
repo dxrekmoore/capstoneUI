1b10d1f2a2e3ebbc69c0027bdce28bfffacaa173
Alex Gadanidis <alex.gadanidis@gmail.com>
2022-03-12 14:22:55 -0500

    adding map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import BaseMap from './components/Basemap';
 function App() {
   return (
     <>
       <div className="App">
         <Navbar/>
+        <BaseMap/>
       </div>
     </>
   );
---
+++ src/components/Basemap.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { loadModules } from 'esri-loader';
+
+const options = {
+  url: 'https://js.arcgis.com/4.6/'
+};
+
+const styles =  {
+  container: {
+    height: '100vh',
+    width: '100vw'
+  },
+  mapDiv: {
+    padding: 0,
+    margin: 0,
+    height: '100%',
+    width: '75%'
+  },
+}
+
+class BaseMap extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: 'loading'
+    }
+  }
+
+  componentDidMount() {
+    loadModules(['esri/Map', 'esri/views/MapView'], options)
+      .then(([Map, MapView]) => {
+        const map = new Map({ basemap: "streets" });
+        const view = new MapView({
+          container: "viewDiv",
+          map,
+          zoom: 15,
+          center: [-76.495, 44.23]
+        });
+        view.then(() => {
+          this.setState({
+            map,
+            view,
+            status: 'loaded'
+          });
+        });
+      })
+
+  }
+
+  renderMap() {
+    if(this.state.status === 'loading') {
+      return <div>loading</div>;
+    }
+  }
+
+  render() {
+
+    return(
+          <div style={styles.container}>
+            <div id='viewDiv' style={ styles.mapDiv } >
+              {this.renderMap()}
+            </div>
+          </div>
+    )
+  }
+}
+
+export default BaseMap
